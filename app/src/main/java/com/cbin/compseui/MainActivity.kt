a61e768cf993629e78f148f5adb9e7cec6a4c1ea
thatCbin <1172816325@qq.com>
2021-04-07 15:17:04 +0800

    更多Compose用法

--- app/src/main/java/com/cbin/compseui/MainActivity.kt
+++ app/src/main/java/com/cbin/compseui/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.cbin.compseui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme.typography
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,35 +32,88 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+
+    private fun btnClick() {
+        RowActivity.start(this)
+    }
+
+    private fun btn2Click() {
+        Toast.makeText(applicationContext, "B", Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * ui
+     */
+    @Preview(showBackground = true, showSystemUi = true)
     @Composable
     fun NewsStory() {
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
-                style = typography.h6,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text =
-                "Davenport, California",
-                style = typography.body2
-            )
-            Text(
-                text = "December 2018",
-                style = typography.body2
-            )
-            Button(
-                onClick = { Toast.makeText(applicationContext, "BBBBBB", Toast.LENGTH_LONG).show() }
-            ) {
+        Column(
+            //verticalArrangement: Arrangement. Vertical = Arrangement . Top,
+            //horizontalAlignment: Alignment.Horizontal = Alignment.Start,
+        ) {
+
+            Column(modifier = Modifier.padding(16.dp)) {
                 Text(
-                    text = "ButtonButtonButtonButton",
+                    text = "A day wandering through the sandhills in Shark Fin Cove, and a few of the sights I saw",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text =
+                    "Davenport, California",
                     style = typography.body2
+                )
+                Text(
+                    text = "December 2018",
+                    style = typography.body2
+                )
+                Spacer(Modifier.height(16.dp))
+            }
+
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Button(
+                    onClick = { btnClick() }
+                ) {
+                    Text(
+                        text = "AAAAAAAA",
+                        style = typography.body2,
+                    )
+                }
+
+                Spacer(Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        btn2Click()
+                    },
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.header),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .height(180.dp)
+                            .fillMaxWidth()
+                            .clip(shape = RoundedCornerShape(10.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                Column(
+                    modifier = Modifier.padding(16.dp),
+                    content = {
+                        Text("Some text")
+                        Text("Some more text")
+                    }
                 )
             }
         }
 
     }
 
-
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, MainActivity::class.java))
+        }
+    }
 }
---
+++ app/src/main/java/com/cbin/compseui/RowActivity.kt
@@ -0,0 +1,123 @@
+package com.cbin.compseui
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.VectorConverter
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.awaitFirstDown
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.tooling.preview.Preview
+import com.cbin.compseui.ui.theme.CompseUiTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
+
+/**
+ * @author Cbin
+ * @CreateDate 2021/4/7
+ * @describe 使用组合布局
+ */
+class RowActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CompseUiTheme {
+                MoveBoxWhereTapped()
+            }
+        }
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun NewUi() {
+        Row(Modifier.padding(10.dp)) {
+            Text(
+                text = "Hello world",
+                // This Text is inside a RowScope so it has access to
+                // Alignment.CenterVertically but not to
+                // Alignment.CenterHorizontally, which would be available
+                // in a ColumnScope.
+                modifier = (Modifier.align(Alignment.CenterVertically)),
+            )
+
+        }
+        Box(
+            modifier = Modifier.drawBehind {
+                // This method accepts a lambda of type DrawScope.() -> Unit
+                // therefore in this lambda we can access properties and functions
+                // available from DrawScope, such as the `drawRectangle` function.
+                drawRect(color = Color.Red)
+            }
+        )
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun MoveBoxWhereTapped() {
+        // Creates an `Animatable` to animate Offset and `remember` it.
+        val animatedOffset = remember {
+            Animatable(Offset(0f, 0f), Offset.VectorConverter)
+        }
+
+        Box(
+            // The pointerInput modifier takes a suspend block of code
+            Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    // Create a new CoroutineScope to be able to create new
+                    // coroutines inside a suspend function
+                    coroutineScope {
+                        while (true) {
+                            // Wait for the user to tap on the screen
+                            val offset = awaitPointerEventScope {
+                                awaitFirstDown().position
+                            }
+                            // Launch a new coroutine to asynchronously animate to where
+                            // the user tapped on the screen
+                            launch {
+                                // Animate to the pressed position
+                                animatedOffset.animateTo(offset)
+                            }
+                        }
+                    }
+                }
+        ) {
+            Text("Tap anywhere", Modifier.align(Alignment.Center))
+            Box(
+                Modifier
+                    .offset {
+                        // Use the animated offset as the offset of this Box
+                        IntOffset(
+                            animatedOffset.value.x.roundToInt(),
+                            animatedOffset.value.y.roundToInt()
+                        )
+                    }
+                    .size(40.dp)
+                    .background(Color(0xff3c1361), CircleShape)
+            )
+        }
+    }
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, RowActivity::class.java))
+        }
+    }
+}
